2fa8120c0a8b90d56bfc2aaf633801181904b0cf
amantrip <amantrip@cs.utsa.edu>
2015-05-12 08:52:31 -0400

    Updated test script

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-array=( tests/test1.jsql tests/test2.jsql )
+
+#Array of Tests
+array=( test_add.jsql test_sub.jsql test_addjson12.jsql test_addjson21.jsql )
+
+#Monitor passed and failed tests
+passed = 0
+failed = 0
+
+
 for i in "${array[@]}"
 do
-	python ../main.py $i > output.txt
-	# Add a comparison script to see if the results matched the output
+	#python ../main.py tests/$i > output.txt
+	if [[ -n $(diff tests/results/result_$i output.txt) ]]; then
+		echo "----- Test "$i" Failed -----"
+		diff tests/results/result_$i output.txt
+		failed=$((failed+1))
+	else
+		echo "------Test "$i" Passed -----"	
+		passed=$((passed+1))
+	fi
 done
 
+echo "------------------------"
+echo "| # of Tests Passed: "$failed" |"
+echo "| # of Tests Failed: "$passed" |"
+echo "------------------------"
 
  
