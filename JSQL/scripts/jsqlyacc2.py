b27faaf040650a96af6173a94aa3a28a926fcbee
RahulGaur <rg2930@columbia.edu>
2015-05-11 23:12:58 -0400

    Script to use

--- JSQL/scripts/jsqlyacc2.py
+++ JSQL/scripts/jsqlyacc2.py
@@ -3,52 +3,54 @@ import codecs
 import subprocess
 from jsqlyacc import yacc
 
+
+fileN = raw_input('Please Enter the jsql file name you want to compile: ')
+file = fileN+'.jsql'
+filebuffer = open(file,'r')
+x = filebuffer.read()
+
 javatemplate = '''
 import java.util.*;
+import org.json.simple.parser.*;
 
 
-public class JSQLClass {
+public class '''+fileN+''' {
 
      __BODY__
 
 }
 '''
 
-for filename in glob.glob1('.','*.jsql'):
-    file = filename
-
-filebuffer = open(file,'r')
-x = filebuffer.read()
-
 try:
     body = yacc.parse(x)
-    #file_name = os.path.splitext("path_to_file")[0]
-    file_name = "JSQLClass.java"
+    file_name = fileN+".java"
     outfile = codecs.open(file_name, encoding = 'utf-8', mode = 'w')
     outfile.write(javatemplate.replace('__BODY__', body))
     outfile.close()
 except Exception as err:
     print err
+    exit()
 
+print file_name
 
-proc = subprocess.Popen('javac JSQLClass.java',
+proc = subprocess.Popen('javac '+file_name,
                         shell=True,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         )
 stdout_value, stderr_value = proc.communicate('new comm')
-#print '\tpass through:', repr(stdout_value)
-#print '\tstderr      :', repr(stderr_value)
 
-if 'double' in str(stderr_value):
-    print "Incompatible types"
+
+
+if 'double' in str(stderr_value) or 'error' in str(stderr_value):
+    print str(stderr_value)
 else:
     print 'Compiled successfully'
 
 
 try:
-    proc1 = subprocess.Popen('java JSQLClass',
+    proc1 = subprocess.Popen('java -cp ~/Documents/PLT/jsql/JSQL/org/json/simple/parser '+fileN,
                         shell=True,
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
